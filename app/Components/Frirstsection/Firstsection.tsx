2e3b5bd7df12b1fee4890f15d5e68c0791453723
syedsaudsaad <s.saud15065@gmail.com>
2024-12-26 15:11:12 +0500

    assignments-4

---
+++ app/Components/Frirstsection/Firstsection.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Firstsection: React.FC = () => {
+  return (
+    <section className="flex flex-col md:flex-row items-center justify-around p-6 bg-white sect">
+        <div className="md:w-1/2 md:pr-4">
+            <h2 className="text-2xl font-bold mb-2 ">IMPECCABLE CRAFTSMANSHIP AND FINESS</h2>
+            <p className="text-gray-700 mb-4">
+        An example of intricate workmanship and detail,
+        elegant necklaces and long and short chains form 
+        a part of our desirable collection.</p>
+            <button className="bg-[#A29875] text-white py-2 px-4 rounded ">Explore Now</button>
+        </div>
+        
+        <div className="mt-6 md:mt-0 md:w-1/2 md:pl-4 ml-96">
+            <img src="/images/s1.jpg" alt="logo" className="w-full md:w-[400px] h-auto rounded-md object-cover"></img>
+        </div>
+    </section>
+  )
+}
+
+export default Firstsection;
---
+++ app/Components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header:React.FC = () => {
+  return (
+    <div>
+    <header className="flex items-center justify-between bg-[#A29875] px-6 py-4">
+<h1 className="text-2xl font-bold  text-white">MANZZARI</h1>
+    </header>
+    </div>
+  )
+}
+
+export default Header;
